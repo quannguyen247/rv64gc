45790d7ad6a7ee6fef8ef195cf43f15b3d697d30
quannguyen247 <nguyendongquan247@gmail.com>
2026-07-05 23:00:36 +0700

    Add fpu64_mul.v

---
+++ Implementation/rtl/modules/fpu64/fpu64_mul.v
@@ -0,0 +1,38 @@
+`timescale 1ns / 1ps
+
+module fpu64_mul (
+    input wire [63:0] rs1,
+    input wire [63:0] rs2,
+    input wire is_double,
+    output reg [63:0] out
+);
+
+    wire [31:0] s1_bits = rs1[31:0];
+    wire [31:0] s2_bits = rs2[31:0];
+
+    shortreal s1_f;
+    shortreal s2_f;
+    shortreal res_f;
+
+    real d1_f;
+    real d2_f;
+    real res_d;
+
+    always @(*) begin
+        s1_f = $bitstoshortreal(s1_bits);
+        s2_f = $bitstoshortreal(s2_bits);
+        d1_f = $bitstoreal(rs1);
+        d2_f = $bitstoreal(rs2);
+    end
+
+    always @(*) begin
+        if (is_double) begin
+            res_d = d1_f * d2_f;
+            out = $realtobits(res_d);
+        end else begin
+            res_f = s1_f * s2_f;
+            out = {32'hFFFFFFFF, $shortrealtobits(res_f)};
+        end
+    end
+
+endmodule
