d7a8f9840cfc0864b0b51e72a1734bd0cafc0761
quannguyen247 <nguyendongquan247@gmail.com>
2026-07-22 11:30:53 +0700

    Optimize mmu64_pte_decode.v

--- Implementation/rtl/modules/mmu64/mmu64_pte_decode.v
+++ Implementation/rtl/modules/mmu64/mmu64_pte_decode.v
@@ -1,8 +1,9 @@
 `timescale 1ns / 1ps
 `include "mmu64_defs.vh"
 
-module mmu64_pte_decode (
+module mmu64_pte_decode(
     input wire [`PTE_WIDTH-1:0] pte_in,
+
     output wire [`PPN_WIDTH-1:0] ppn,
     output wire [25:0] ppn2,
     output wire [8:0] ppn1,
@@ -24,21 +25,22 @@ module mmu64_pte_decode (
     assign ppn0 = pte_in[`PTE_PPN0];
     assign ppn1 = pte_in[`PTE_PPN1];
     assign ppn2 = pte_in[`PTE_PPN2];
-    assign ppn = pte_in[`PTE_PPN];
+    assign ppn  = pte_in[`PTE_PPN];
 
-    assign valid = pte_in[`PTE_V];
-    assign readable = pte_in[`PTE_R];
-    assign writable = pte_in[`PTE_W];
-    assign executable = pte_in[`PTE_X];
-    assign user_mode = pte_in[`PTE_U];
+    assign valid       = pte_in[`PTE_V];
+    assign readable    = pte_in[`PTE_R];
+    assign writable    = pte_in[`PTE_W];
+    assign executable  = pte_in[`PTE_X];
+    assign user_mode   = pte_in[`PTE_U];
     assign global_flag = pte_in[`PTE_G];
-    assign accessed = pte_in[`PTE_A];
-    assign dirty = pte_in[`PTE_D];
+    assign accessed    = pte_in[`PTE_A];
+    assign dirty       = pte_in[`PTE_D];
 
-    assign rsw = pte_in[`PTE_RSW];
-    assign rsvd_bits = pte_in[`PTE_RSVD];
+    assign rsw        = pte_in[`PTE_RSW];
+    assign rsvd_bits  = pte_in[`PTE_RSVD];
 
-    assign is_leaf = valid & (readable | executable);
+    assign is_leaf    = valid & (readable | executable);
+
     assign is_pointer = valid & ~readable & ~writable & ~executable;
 
 endmodule
